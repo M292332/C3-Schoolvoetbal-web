69aea2590dfe4d56974c9da7c5b11486f233826b
levistinkt <sdestinkerd@gmail.com>
2024-12-05 14:06:01 +0100

    Revert "user account seeder"
    This reverts commit cfaa1a10c3e93b626c6b3a9439dd98fb379d8e51.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
             TeamSeeder::class,
             PlayerSeeder::class,
             ToernooiSeeder::class,
-            UserSeeder::class,
 
         ]);
     }
