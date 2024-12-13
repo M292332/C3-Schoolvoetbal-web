944901a6ca515f9b56294e57f5cadc2e2c681ee7
bayazid72 <bayazidhatem@gmail.com>
2024-12-13 12:23:05 +0100

    Logo
    Het logo is toegevoegd aan de header als onderdeel van de body

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,17 @@
 <div class="container mx-auto flex flex-col sm:flex-row justify-between items-center px-4 sm:px-6">
-    <h1 class="text-2xl sm:text-3xl font-bold text-center sm:text-left">
+    <h1 class="text-2xl sm:text-3xl font-bold text-center sm:text-left flex items-center space-x-4">
         <span>FootyGokken</span>
+        <video class="w-24 md:w-32 lg:w-40 h-auto" autoplay loop muted>
+            <source src="{{ asset('3.mp4') }}" type="video/mp4">
+        </video>
     </h1>
+
     <nav class="mt-4 sm:mt-0">
         <ul class="flex space-x-4">
             <!-- Dropdown menu  -->
             <li class="dropdown">
-                <a href="#" class="hover:text-blue-300 transition">Opties</a>
+                <a href="#" class="text-2xl sm:text-3xl font-bold text-center sm:text-left flex items-center space-x-4"">Opties</a>
+
                 <div class="dropdown-content">
                     <a href="/" class="dropdown-item">Home</a>
 
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,18 @@
 <x-base-layout>
     @auth
-    <div class="flex justify-center items-center h-[40vh]">
+    <div class="flex flex-col justify-center items-center h-[40vh]">
         <p class="text-xl font-semibold">Alright, let's go gamble!</p>
+        <video class="w-24 md:w-32 lg:w-40 h-auto" autoplay loop muted>
+            <source src="{{ asset('2.mp4') }}" type="video/mp4">
+        </video>
     </div>
     @else
-    <div class="flex justify-center items-center h-[40vh]">
+    <div class="flex flex-col justify-center items-center h-[40vh] space-y-4">
         <p class="text-xl font-semibold">Ready to gamble?</p>
+        <!-- Video under the text -->
+        <video class="w-24 md:w-32 lg:w-40 h-auto" autoplay loop muted>
+            <source src="{{ asset('2.mp4') }}" type="video/mp4">
+        </video>
     </div>
     @endauth
 </x-base-layout>
